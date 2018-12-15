843bc05a7bb33383ca6d709ef762db3552652a8c
codekiller <spartdesign972@gmail.com>
2018-12-15 02:54:39 -0400

    add menu>>>not finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
-import { AboutComponent } from "./about/about.component";
-import { MyWorksComponent } from "./my-works/my-works.component";
-import { ContactComponent } from "./contact/contact.component";
-import { HomeComponent } from "./home/home.component";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MyWorksComponent } from './my-works/my-works.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: "", component: HomeComponent },
-  { path: "about", component: AboutComponent },
-  { path: "my-works", component: MyWorksComponent },
-  { path: "contact", component: ContactComponent }
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'my-works', component: MyWorksComponent },
+    { path: 'contact', component: ContactComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+    selector: 'app-menu',
+    templateUrl: './menu.component.html',
+    styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+    public toggle: Boolean = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    toggleMenu(delai: number) {
+        setTimeout(() => {
+            this.toggle = !this.toggle;
+        }, delai);
+    }
 }
