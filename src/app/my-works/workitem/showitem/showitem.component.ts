7986cf86f5517b72ab350edc782748e136c94226
codekiller <spartdesign972@gmail.com>
2019-01-02 13:23:48 -0400

    add description to projects

--- src/app/my-works/workitem/showitem/showitem.component.ts
+++ src/app/my-works/workitem/showitem/showitem.component.ts
@@ -15,12 +15,15 @@ export class ShowitemComponent implements OnChanges {
     @Input() imgInfos;
 
     public imgUrl: string;
+    public imgDesc: string;
     public toggleShow = true;
 
     constructor() {}
 
     ngOnChanges(changes: SimpleChanges) {
+        // console.log(changes);
         this.imgUrl = changes.imgInfos.currentValue.imgUrl;
+        this.imgDesc = changes.imgInfos.currentValue.imgDesc;
         this.toggleShow = changes.imgInfos.currentValue.showImg;
     }
 
--- src/app/my-works/workitem/workitem.component.ts
+++ src/app/my-works/workitem/workitem.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 interface ImgInfos {
     imgUrl: string;
+    imgDesc: string;
     showImg: boolean;
 }
 @Component({
@@ -11,23 +12,26 @@ interface ImgInfos {
 export class WorkitemComponent implements OnChanges {
     @Input() workItems: object;
 
-    public imgInfos: ImgInfos;
+    public imgInfos: ImgInfos = {
+        imgUrl: '',
+        imgDesc: '',
+        showImg: true,
+    };
+
     public show = false;
 
     constructor() {}
 
     ngOnChanges(change: SimpleChanges) {
         this.show = false;
-        // this.imgInfos = {
-        //     imgUrl: '',
-        //     showImg: true,
-        // };
         setTimeout(() => (this.show = true), 200);
     }
 
-    sendInfo($event) {
+    sendInfo(url, desc) {
+        // console.log({ url, desc });
         this.imgInfos = {
-            imgUrl: $event,
+            imgUrl: url,
+            imgDesc: desc,
             showImg: false,
         };
         // console.log(this.imgInfos);
